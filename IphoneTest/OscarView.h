3682f15d0c99ae578a668cf0f4edd558dc345562
oscart <oscar.demoya@koombea.com>
2011-11-08 16:45:51 -0500

    Added avfoundation framework

---
+++ IphoneTest/OscarView.h
@@ -0,0 +1,13 @@
+//
+//  OscarView.h
+//  IphoneTest
+//
+//  Created by Oscar De Moya on 11/8/11.
+//  Copyright (c) 2011 Koombea S.A.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OscarView : UIViewController
+
+@end
