c56f52cd2b522c9d126697f020496fa73d4826fa
Luis Alberto Tovar Taboada <luis.tovar@koombea.com>
2012-04-24 09:43:42 -0500

    added view controller

---
+++ IphoneTest/LTViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTViewController.h
+//  IphoneTest
+//
+//  Created by luis alberto tovar taboada on 24/04/12.
+//  Copyright (c) 2012 koombea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTViewController : UINavigationController
+
+@end
