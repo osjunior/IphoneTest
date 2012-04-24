d5fce04c8b14ac27a29a90b999999cd1f46837bf
Oscar De Moya <oscardemoya@KoobCamOrp.local>
2012-04-24 10:08:10 -0500

    Test o

---
+++ IphoneTest/OTest.h
@@ -0,0 +1,13 @@
+//
+//  OTest.h
+//  IphoneTest
+//
+//  Created by Oscar De Moya on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTest : UIViewController
+
+@end
---
+++ IphoneTest/OTest.m
@@ -0,0 +1,43 @@
+//
+//  OTest.m
+//  IphoneTest
+//
+//  Created by Oscar De Moya on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "OTest.h"
+
+@interface OTest ()
+
+@end
+
+@implementation OTest
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
