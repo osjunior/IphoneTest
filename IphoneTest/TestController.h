44810e91060bfb0467893d59c1853854097d5dd7
Oscar Rodriguez <oscar_rodriguez_2010@hotmail.com>
2012-04-24 09:53:58 -0500

    add test controller

---
+++ IphoneTest/TestController.h
@@ -0,0 +1,13 @@
+//
+//  TestController.h
+//  IphoneTest
+//
+//  Created by Oscar Rodriguez on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestController : UIViewController
+
+@end
