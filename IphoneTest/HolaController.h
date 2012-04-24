fc7ab29b03a4481f553c281d2727f60a9fddfb49
Oscar Rodriguez <oscar_rodriguez_2010@hotmail.com>
2012-04-24 09:48:13 -0500

    update

---
+++ IphoneTest/HolaController.h
@@ -0,0 +1,13 @@
+//
+//  HolaController.h
+//  IphoneTest
+//
+//  Created by Oscar Rodriguez on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HolaController : UIViewController
+
+@end
---
+++ IphoneTest/HolaController.m
@@ -0,0 +1,44 @@
+//
+//  HolaController.m
+//  IphoneTest
+//
+//  Created by Oscar Rodriguez on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HolaController.h"
+
+@interface HolaController ()
+
+@end
+
+@implementation HolaController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
